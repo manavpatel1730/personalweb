103ba0a9326fe4962411bc58e5940e3118abb62f
manavpatel1730 <manav1730@gmail.com>
2024-03-09 19:43:56 +0530

    contact

--- src/app/masterpages/contact/contact-us/contact-us.component.ts
+++ src/app/masterpages/contact/contact-us/contact-us.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent {
+form : any;
+submitted = false;
+success = false;
+error = false;
+message = '';
 
+
+  constructor(public formBuilder : FormBuilder) { }
+
+  ngOnInit() {
+this.form = this.formBuilder.group({
+  firstName: ['', Validators.required],
+  lastName: ['', [Validators.required, Validators.email]],
+  email: ['', Validators.required],
+  phone: ['', Validators.required],
+  Address: ['', Validators.required],
+  City: ['', Validators.required],
+  state: ['', Validators.required],
+  checkOut: ['', Validators.required],
+
+
+  });
+}
 }
--- src/app/masterpages/masterpage.module.ts
+++ src/app/masterpages/masterpage.module.ts
@@ -5,6 +5,7 @@ import { MasterpagerRoutingModule } from './masterpager-routing.module';
 import { AboutUsComponent } from './about/about-us/about-us.component';
 import { ContactUsComponent } from './contact/contact-us/contact-us.component';
 import { TeamComponent } from './team/team/team.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -18,7 +19,8 @@ import { TeamComponent } from './team/team/team.component';
   ],
   imports: [
     CommonModule,
-    MasterpagerRoutingModule
+    MasterpagerRoutingModule,
+    ReactiveFormsModule
     ]
 })
 export class MasterpageModule {
